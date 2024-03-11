48afec5815844e5b73dfdf39faedd1bed4e07de8
BrilliantTreasure1 <brillianttrussure@gmail.com>
2024-03-11 01:48:51 -0700

    configuration Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { ThemeProvider,createTheme } from "@mui/material/styles"
+import rtlPlugin from "stylis-plugin-rtl"
+import { CacheProvider } from "@emotion/react"
+import { HelmetProvider,Helmet } from "react-helmet-async"
+import createcache from "@emotion/cache"
+import { prefixer } from "stylis"
+
 import './App.css';
+
+
+//NOTE create custom theme
+const theme = createTheme({
+  direction : rtl,
+  typography : {
+    fontFamily : "vazir"
+  }
+})
+
+//NOTE create rtl cache
+const cacheRTL = createcache({
+  key :"muirtl"
+  ,stylisPlugins: [prefixer,rtlPlugin]
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={theme}>
+        <HelmetProvider>
+          <Helmet>
+            <title>BrilliantTreasur</title>
+          </Helmet>
+        </HelmetProvider>
+          <div className="App">
+       
+          </div>
+  
+    </ThemeProvider>
+    </CacheProvider>
+    
+);
 }
 
 export default App;
